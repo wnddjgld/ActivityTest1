a46a8aa56f70481d95f18c730eec23aa43a4ed4a
wnddjgld <wnddjgld@gmail.com>
2025-06-23 15:28:03 +0900

    직접풀어보기 10-1

--- app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    RadioButton rbSecond, rbThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +28,23 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        rg = findViewById(R.id.rg);
+        rbSecond = findViewById(R.id.rb_second);
+        rbSecond.setChecked(true);
+
         Button btnMain = findViewById(R.id.btn_main);
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                Intent intent = null;
+                if(rg.getCheckedRadioButtonId() ==  R.id.rb_second)
+                    intent = new Intent(getApplicationContext(), SecondActivity.class);
+                else
+                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
+
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/kr/ac/kopo/activitytest1/ThirdActivity.java
+++ app/src/main/java/kr/ac/kopo/activitytest1/ThirdActivity.java
@@ -1,4 +1,33 @@
 package kr.ac.kopo.activitytest1;
 
-public class ThirdActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnThird = findViewById(R.id.btn_third);
+        btnThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
